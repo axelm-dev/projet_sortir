2c748c65bb0db36c2bdd524f8f56ec8f9388c774
François Michaël <91324853+FrancoisMoanaMichael@users.noreply.github.com>
2024-02-21 13:43:21 +0100

    FIX SIGNIN

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -17,11 +19,15 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('campus',EntityType::class,[
+                'class'=>Campus::class,
+                'choice_label' => 'name',
+                'placeholder'=>'choisir un campus',
+                'required'=>true,
+            ])
             ->add('login')
             ->add('email')
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
