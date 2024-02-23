d158596f7dc14c890bc2f6ae7da5b9a177a513a7
marietournelle35 <marie.benoit2023@campus-eni.fr>
2024-02-23 14:49:58 +0100

    Factories / fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CampusFactory;
+use App\Factory\CityFactory;
+use App\Factory\MeetingFactory;
+use App\Factory\PlaceFactory;
+use App\Factory\ProfileFactory;
+use App\Factory\StateMeetingFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,8 +17,28 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        UserFactory::createOne();
+        UserFactory::createMany(10);
         UserFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+
+        ProfileFactory::createMany(10);
+
+        CityFactory::createMany(10);
+
+
+        $cities = CityFactory::randomSet(5);
+        CampusFactory::createMany(5, fn() => ['name' => $cities[CityFactory::faker()->unique()->numberBetween(0, 4)]->getName()]);
+
+
+        PlaceFactory::createMany(10);
+
+        StateMeetingFactory::createOne(['value' => 'Créée']);
+        StateMeetingFactory::createOne(['value' => 'Publiée']);
+        StateMeetingFactory::createOne(['value' => 'Clôturée']);
+        StateMeetingFactory::createOne(['value' => "Activité en cours"]);
+        StateMeetingFactory::createOne(['value' => 'Passée']);
+        StateMeetingFactory::createOne(['value' => 'Annulée']);
+
+        MeetingFactory::createMany(50);
 
         $manager->flush();
     }
