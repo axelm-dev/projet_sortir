c30c6f058035c3acb1bceef8b5cf27eef5508712
axelm-dev <axel.mainguy2022@campus-eni.fr>
2024-02-28 11:39:39 +0100

    Service Security add check User

--- src/Controller/PermAppInterface.php
+++ src/Controller/PermAppInterface.php
@@ -7,7 +7,6 @@ interface PermAppInterface
     public const PERM_MEETING_EDIT = "EDIT_MEETING";
     public const PERM_MEETING_DELETE = "DELETE_MEETING";
     public const PERM_MEETING_VIEW = "VIEW_MEETING";
-    public const PERM_MEETING_CREATE = "CREATE_MEETING";
     public const PERM_MEETING_CLOSE = "CLOSE_MEETING";
     public const PERM_MEETING_OPEN = "OPEN_MEETING";
     public const PERM_MEETING_CANCEL = "CANCEL_MEETING";
@@ -15,4 +14,9 @@ interface PermAppInterface
     public const PERM_MEETING_PUBLISH = "PUBLISH_MEETING";
 
     public const PERM_MEETING_NEW = "NEW_MEETING";
+    public const PERM_PLACE_EDIT = "EDIT_PLACE";
+    public const PERM_PLACE_DELETE = "DELETE_PLACE";
+    public const PERM_PLACE_VIEW = "VIEW_PLACE";
+    public const PERM_PLACE_NEW = "NEW_PLACE";
+
 }
--- src/Service/AuthorizationService.php
+++ src/Service/AuthorizationService.php
@@ -10,7 +10,6 @@ class AuthorizationService implements PermAppInterface
 {
     private AuthorizationCheckerInterface $authorizationChecker;
 
-    private ProjectController $projectController;
 
     public function __construct(AuthorizationCheckerInterface $authorizationChecker)
     {
@@ -21,12 +20,19 @@ class AuthorizationService implements PermAppInterface
     {
         return match ($attributes) {
             'EDIT_USER' => $this->canEditUser($user, $object),
-            'EDIT_MEETING' => $this->canEditMeeting($user, $object),
+            // MEETING
+            self::PERM_MEETING_EDIT => $this->canEditMeeting($user, $object),
             self::PERM_MEETING_VIEW => $this->canViewMeeting($user, $object),
             self::PERM_MEETING_CANCEL => $this->canCancelMeeting($user, $object),
-            'PUBLISH_MEETING' => $this->canPublishMeeting($user,$object),
-            'NEW_MEETING' => $this->canNewMeeting($user, $object),
-            'DELETE_MEETING' => $this->canDeleteMeeting($user, $object),
+            self::PERM_MEETING_PUBLISH => $this->canPublishMeeting($user,$object),
+            self::PERM_MEETING_NEW => $this->canNewMeeting($user, $object),
+            self::PERM_MEETING_DELETE => $this->canDeleteMeeting($user, $object),
+            // PLACE
+            self::PERM_PLACE_EDIT => $this->canEditPlace($user, $object),
+            self::PERM_PLACE_DELETE => $this->canDeletePlace($user, $object),
+            self::PERM_PLACE_VIEW => $this->canViewPlace($user, $object),
+            self::PERM_PLACE_NEW => $this->canNewPlace($user, $object),
+
             default => false,
         };
     }
@@ -44,10 +50,10 @@ class AuthorizationService implements PermAppInterface
         return $this->authorizationChecker->isGranted('ROLE_ADMIN');
     }
 
-    private function canEditMeeting($meeting): bool
+    private function canEditMeeting($user, $meeting): bool
     {
-        if ($this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY') === false) {
-            return false;
+        if($user instanceof AppUser) {
+            return true;
         }
 
         if($meeting->getOrganizer()->getId() === $this->authorizationChecker->getUser()->getId()) {
@@ -79,10 +85,10 @@ class AuthorizationService implements PermAppInterface
         return $this->authorizationChecker->isGranted('ROLE_ADMIN');
     }
 
-    private function canPublishMeeting($meeting): bool
+    private function canPublishMeeting($user, $meeting): bool
     {
-        if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return false;
+        if($user instanceof AppUser) {
+            return true;
         }
 
         if($meeting->getOrganizer()->getId() === $this->authorizationChecker->getUser()->getId()) {
@@ -92,22 +98,54 @@ class AuthorizationService implements PermAppInterface
         return $this->authorizationChecker->isGranted('ROLE_ADMIN');
     }
 
-    private function canNewMeeting($meeting): bool
+    private function canNewMeeting($user, $meeting): bool
     {
-        if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return false;
+        if($user instanceof AppUser) {
+            return true;
         }
 
         return true;
     }
 
-    private function canDeleteMeeting(mixed $object): bool
+    private function canDeleteMeeting($user, $object): bool
     {
-        if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return false;
+        if($user instanceof AppUser) {
+            return true;
         }
 
         if($object->getOrganizer()->getId() === $this->authorizationChecker->getUser()->getId()) {
+            return true;
+        }
+
+        return $this->authorizationChecker->isGranted('ROLE_ADMIN');
+    }
+
+    private function canEditPlace($user, mixed $object): bool
+    {
+        if($user instanceof AppUser) {
+            return true;
+        }
+
+        return $this->authorizationChecker->isGranted('ROLE_ADMIN');
+    }
+
+    private function canDeletePlace($user, mixed $object): bool
+    {
+        return $this->authorizationChecker->isGranted('ROLE_ADMIN');
+    }
+
+    private function canViewPlace($user, mixed $object): bool
+    {
+        if($user instanceof AppUser) {
+            return true;
+        }
+
+        return $this->authorizationChecker->isGranted('ROLE_ADMIN');
+    }
+
+    private function canNewPlace($user, mixed $object): bool
+    {
+        if($user instanceof AppUser) {
             return true;
         }
 
