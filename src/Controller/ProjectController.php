0a202c06461d8cd427c093e60711e3343e98e936
axelm-dev <axel.mainguy2022@campus-eni.fr>
2024-02-28 17:07:23 +0100

    Const Perm & State

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,18 +4,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ProjectController extends AbstractController implements PermAppInterface
+class ProjectController extends AbstractController implements PermAndStateAppInterface
 {
-
-    public const STATE_MEETING_CREATED = "Créée";
-    public const STATE_MEETING_OPENED = "Ouverte";
-    public const STATE_MEETING_CLOSED = "Clôturée";
-    public const STATE_MEETING_ACTIVITY = "Activité en cours";
-    public const STATE_MEETING_PASSED = "Passée";
-    public const STATE_MEETING_ARCHIVED = "Archivée";
-    public const STATE_MEETING_CANCELED = "Annulée";
-
-
-
-
 }
--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,13 +2,14 @@
 
 namespace App\Twig;
 
+use App\Controller\PermAndStateAppInterface;
 use App\Entity\Meeting;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 #[AutoconfigureTag('twig.extension')]
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements PermAndStateAppInterface
 {
     public function getFunctions(): array
     {
@@ -21,7 +22,7 @@ class AppExtension extends AbstractExtension
     public function isButtonsInscriptionDisplayed(Meeting $meeting): bool
     {
         $dateNow = new \DateTime('now');
-        return $meeting->getState()->getValue() === 'Ouverte' || ($meeting->getState()->getValue() ===  'Clôturée' && $dateNow < $meeting->getLimitDate());
+        return $meeting->getState()->getValue() === self::STATE_MEETING_OPENED || ($meeting->getState()->getValue() ===  self::STATE_MEETING_CLOSED && $dateNow < $meeting->getLimitDate());
     }
 
     public function isButtonModifyAndCancelDisplayed(Meeting $meeting) : bool
