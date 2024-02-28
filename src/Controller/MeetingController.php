d8ddecdfa958e2aec454df069ef6986e050c5113
marietournelle35 <marie.benoit2023@campus-eni.fr>
2024-02-28 15:02:26 +0100

    form place new in form meeting

--- src/Controller/MeetingController.php
+++ src/Controller/MeetingController.php
@@ -18,6 +18,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use function MongoDB\BSON\toJSON;
 
 #[IsGranted('ROLE_USER')]
 #[Route('/meeting')]
