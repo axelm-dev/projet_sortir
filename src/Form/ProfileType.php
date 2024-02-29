444002b89ee89b3a556ccc6c8c089a07be5d8bc3
marietournelle35 <marie.benoit2023@campus-eni.fr>
2024-02-29 17:02:44 +0100

    CSS meeting_new

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -6,6 +6,7 @@ use App\Entity\Profile;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,14 +15,22 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phone')
-            ->add('imageFile',FileType::class)
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('imageFile',FileType::class, [
+                'label' => 'Image de profil'
+            ])
             ->add('modifier',SubmitType::class,[
-                'label' => 'Modifier',
+                'label' => 'Enregistrer',
                 'attr' => [
-                    'class' => 'btn btn-primary'
+                    'class' => 'btn btn-custom'
                 ]
             ])
         ;
